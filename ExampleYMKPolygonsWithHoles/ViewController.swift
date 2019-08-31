f8322b573a82a2117dbd28c521eca60b8f6f8209
Станислав Шияновский <st-small@rambler.ru>
2019-08-31 16:18:58 +0300

    Initial commit.

---
+++ ExampleYMKPolygonsWithHoles/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ExampleYMKPolygonsWithHoles
+//
+//  Created by Станислав Шияновский on 8/31/19.
+//  Copyright © 2019 Станислав Шияновский. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
